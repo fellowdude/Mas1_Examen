fe0f45e022a438778514601ed9a3b417be599f77
fellowdude <fellowdude@users.noreply.github.com>
2024-05-30 18:19:20 -0500

    routing(~): updated routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'category/:name',
+    loadComponent: () => import('./pages/category/category.component').then((m) => m.CategoryComponent),
+  },
+];
